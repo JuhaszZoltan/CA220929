e0c2723547ad6f4160bbb1d8f0e49c1684682ed6
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2022-09-29 09:46:33 +0200

    Add project files.

---
+++ Mission.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA220929
+{
+    internal class Mission
+    {
+        public string Code { get; set; }
+        public DateTime LaunchDate { get; set; }
+        public string ShuttleName { get; set; }
+        private int SpaceTimeDays { get; set; }
+        private int SpaceTimeHours { get; set; }
+        public int TotalSpaceHours => SpaceTimeDays * 24 + SpaceTimeHours;
+        public string LandingAirbase { get; set; }
+        public int NOCrew { get; set; }
+
+        public Mission(string row)
+        {
+            var pieces = row.Split(';');
+            Code = pieces[0];
+            LaunchDate = DateTime.Parse(pieces[1]);
+            ShuttleName = pieces[2];
+            SpaceTimeDays = int.Parse(pieces[3]);
+            SpaceTimeHours = int.Parse(pieces[4]);
+            LandingAirbase = pieces[5];
+            NOCrew = int.Parse(pieces[6]);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using CA220929;
+using System.Text;
+
+List<Mission> missions = new();
+using StreamReader sr = new(@"..\..\..\src\kuldetesek.csv", Encoding.UTF8);
+while (!sr.EndOfStream) missions.Add(new Mission(sr.ReadLine()));
+
+ParhozamosanKezeltListakkal(missions);
+Console.WriteLine("--------------");
+AsszociativDinamikusVektorral(missions);
+Console.WriteLine("--------------");
+NyelvbeIntegraltLekerdezessel(missions);
+
+static void ParhozamosanKezeltListakkal(List<Mission> missions)
+{
+    List<string> names = new();
+    List<int> sumOfhours = new();
+
+    for (int i = 0; i < missions.Count; i++)
+    {
+        if (!Contains(missions[i].ShuttleName))
+        {
+            names.Add(missions[i].ShuttleName);
+            sumOfhours.Add(missions[i].TotalSpaceHours);
+        }
+        else
+        {
+            int index = IndexOf(missions[i].ShuttleName);
+            sumOfhours[index] += missions[i].TotalSpaceHours;
+        }
+    }
+
+    for (int i = 0; i < names.Count; i++)
+    {
+        Console.WriteLine($"{names[i]} - {sumOfhours[i] / 24F}");
+    }
+
+    bool Contains(string shuttleName)
+    {
+        int i = 0;
+        while (i < names.Count && names[i] != shuttleName)
+        {
+            i++;
+        }
+        if (i < names.Count) return true;
+        else return false;
+    }
+
+    int IndexOf(string shuttleName)
+    {
+        int i = 0;
+        while (names[i] != shuttleName)
+        {
+            i++;
+        }
+        return i;
+    }
+}
+static void AsszociativDinamikusVektorral(List<Mission> missions)
+{
+    Dictionary<string, int> dictionary = new();
+
+    foreach (var m in missions)
+    {
+        if (!dictionary.ContainsKey(m.ShuttleName))
+            dictionary[m.ShuttleName] = m.TotalSpaceHours;
+        else
+            dictionary[m.ShuttleName] += m.TotalSpaceHours;
+    }
+
+    foreach (var kvp in dictionary)
+    {
+        Console.WriteLine($"{kvp.Key} - {kvp.Value / 24F}");
+    }
+}
+static void NyelvbeIntegraltLekerdezessel(List<Mission> missions)
+{
+    var res = missions.GroupBy(m => m.ShuttleName);
+    foreach (var item in res)
+    {
+        Console.WriteLine($"{item.Key} - {item.Sum(m => m.TotalSpaceHours) / 24F}");
+    }
+}
